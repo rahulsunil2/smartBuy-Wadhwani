b9d10924ec2a9aa2e96db365a5b4cf27835a60f2
Rahul Sunil <rahulsunil2@gmail.com>
2021-06-01 21:16:31 +0530

    Added Video.html

--- src/views/Landing.js
+++ src/views/Landing.js
@@ -105,9 +105,11 @@ export default function Landing() {
                                         <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-lightBlue-400">
                                             <i className="fas fa-retweet"></i>
                                         </div>
-                                        <h6 className="text-xl font-semibold">
-                                            Video Calls
-                                        </h6>
+                                        <a href="./video.html">
+                                            <h6 className="text-xl font-semibold">
+                                                Video Calls
+                                            </h6>
+                                        </a>
                                         <p className="mt-2 mb-4 text-blueGray-500">
                                             Get more personalised help through
                                             video call sessions.
@@ -197,96 +199,6 @@ export default function Landing() {
                         </div>
                     </div>{" "}
                 </section>
-                {/* <section className="relative py-20">
-                    <div
-                        className="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20 h-20"
-                        style={{ transform: "translateZ(0)" }}>
-                        <svg
-                            className="absolute bottom-0 overflow-hidden"
-                            xmlns="http://www.w3.org/2000/svg"
-                            preserveAspectRatio="none"
-                            version="1.1"
-                            viewBox="0 0 2560 100"
-                            x="0"
-                            y="0">
-                            <polygon
-                                className="text-white fill-current"
-                                points="2560 0 2560 100 0 100"></polygon>
-                        </svg>
-                    </div>
-                    <div className="container mx-auto px-4">
-                        <div className="items-center flex flex-wrap">
-                            <div className="w-full md:w-4/12 ml-auto mr-auto px-4">
-                                <img
-                                    alt="..."
-                                    className="max-w-full rounded-lg shadow-lg"
-                                    src="https://images.unsplash.com/photo-1555212697-194d092e3b8f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
-                                />
-                            </div>
-                            <div className="w-full md:w-5/12 ml-auto mr-auto px-4">
-                                <div className="md:pr-12">
-                                    <div className="text-lightBlue-600 p-3 text-center inline-flex items-center justify-center w-16 h-16 mb-6 shadow-lg rounded-full bg-lightBlue-300">
-                                        <i className="fas fa-rocket text-xl"></i>
-                                    </div>
-                                    <h3 className="text-3xl font-semibold">
-                                        A growing company
-                                    </h3>
-                                    <p className="mt-4 text-lg leading-relaxed text-blueGray-500">
-                                        The extension comes with three pre-built
-                                        pages to help you get started faster.
-                                        You can change the text and images and
-                                        you're good to go.
-                                    </p>
-                                    <ul className="list-none mt-6">
-                                        <li className="py-2">
-                                            <div className="flex items-center">
-                                                <div>
-                                                    <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-lightBlue-600 bg-lightBlue-200 mr-3">
-                                                        <i className="fas fa-fingerprint"></i>
-                                                    </span>
-                                                </div>
-                                                <div>
-                                                    <h4 className="text-blueGray-500">
-                                                        Carefully crafted
-                                                        components
-                                                    </h4>
-                                                </div>
-                                            </div>
-                                        </li>
-                                        <li className="py-2">
-                                            <div className="flex items-center">
-                                                <div>
-                                                    <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-lightBlue-600 bg-lightBlue-200 mr-3">
-                                                        <i className="fab fa-html5"></i>
-                                                    </span>
-                                                </div>
-                                                <div>
-                                                    <h4 className="text-blueGray-500">
-                                                        Amazing page examples
-                                                    </h4>
-                                                </div>
-                                            </div>
-                                        </li>
-                                        <li className="py-2">
-                                            <div className="flex items-center">
-                                                <div>
-                                                    <span className="text-xs font-semibold inline-block py-1 px-2 uppercase rounded-full text-lightBlue-600 bg-lightBlue-200 mr-3">
-                                                        <i className="far fa-paper-plane"></i>
-                                                    </span>
-                                                </div>
-                                                <div>
-                                                    <h4 className="text-blueGray-500">
-                                                        Dynamic components
-                                                    </h4>
-                                                </div>
-                                            </div>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>{" "}
-                    </div>{" "}
-                </section> */}
                 <section className="pt-20 pb-48">
                     <div className="container mx-auto px-4">
                         <div className="flex flex-wrap justify-center text-center mb-24">
@@ -337,62 +249,7 @@ export default function Landing() {
                                 points="2560 0 2560 100 0 100"></polygon>
                         </svg>
                     </div>
-                    <div className="container mx-auto px-4 lg:pt-24 lg:pb-64">
-                        {/* <div className="flex flex-wrap text-center justify-center">
-                            <div className="w-full lg:w-6/12 px-4">
-                                <h2 className="text-4xl font-semibold text-white">
-                                    Build something
-                                </h2>
-                                <p className="text-lg leading-relaxed mt-4 mb-4 text-blueGray-400">
-                                    Put the potentially record low maximum sea
-                                    ice extent tihs year down to low ice.
-                                    According to the National Oceanic and
-                                    Atmospheric Administration, Ted, Scambos.
-                                </p>
-                            </div>
-                        </div>{" "}
-                        <div className="flex flex-wrap mt-12 justify-center">
-                            <div className="w-full lg:w-3/12 px-4 text-center">
-                                <div className="text-blueGray-800 p-3 w-12 h-12 shadow-lg rounded-full bg-white inline-flex items-center justify-center">
-                                    <i className="fas fa-medal text-xl"></i>
-                                </div>
-                                <h6 className="text-xl mt-5 font-semibold text-white">
-                                    Excelent Services
-                                </h6>
-                                <p className="mt-2 mb-4 text-blueGray-400">
-                                    Some quick example text to build on the card
-                                    title and make up the bulk of the card's
-                                    content.
-                                </p>
-                            </div>{" "}
-                            <div className="w-full lg:w-3/12 px-4 text-center">
-                                <div className="text-blueGray-800 p-3 w-12 h-12 shadow-lg rounded-full bg-white inline-flex items-center justify-center">
-                                    <i className="fas fa-poll text-xl"></i>
-                                </div>{" "}
-                                <h5 className="text-xl mt-5 font-semibold text-white">
-                                    Grow your market{" "}
-                                </h5>{" "}
-                                <p className="mt-2 mb-4 text-blueGray-400">
-                                    Some quick example text to build on the card
-                                    title and make up the bulk of the card 's
-                                    content.{" "}
-                                </p>{" "}
-                            </div>{" "}
-                            <div className="w-full lg:w-3/12 px-4 text-center">
-                                <div className="text-blueGray-800 p-3 w-12 h-12 shadow-lg rounded-full bg-white inline-flex items-center justify-center">
-                                    <i className="fas fa-lightbulb text-xl"></i>
-                                </div>{" "}
-                                <h5 className="text-xl mt-5 font-semibold text-white">
-                                    Launch time{" "}
-                                </h5>{" "}
-                                <p className="mt-2 mb-4 text-blueGray-400">
-                                    Some quick example text to build on the card
-                                    title and make up the bulk of the card 's
-                                    content.{" "}
-                                </p>{" "}
-                            </div>{" "}
-                        </div>{" "}*/}
-                    </div>{" "}
+                    <div className="container mx-auto px-4 lg:pt-24 lg:pb-64"></div>{" "}
                 </section>{" "}
                 <section className="relative block py-24 lg:pt-0 bg-blueGray-800">
                     <div className="container mx-auto px-4">
@@ -413,10 +270,6 @@ export default function Landing() {
                                             <h4 className="text-2xl font-semibold">
                                                 Want to Know More?
                                             </h4>
-                                            {/* <p className="leading-relaxed mt-1 mb-4 text-blueGray-500">
-                                            Complete this form and we will get
-                                            back to you in 24 hours.
-                                        </p> */}
                                             <div className="relative w-full mb-3 mt-8">
                                                 <label
                                                     className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
